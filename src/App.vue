a5fda2e967cb0dc0a39a334ce11d0e19e72bcb96
Kareem Aljabr <kareem.aljabr@universalorlando.com>
2020-06-24 14:34:45 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Display/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Display from './components/Display.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Display
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url(http://fonts.googleapis.com/css?family=Ubuntu);
+  @import url(http://fonts.googleapis.com/css?family=Lato:400,700);
+  body {
+    font-family: 'Lato', sans-serif;
+    font-size: 14pt;
+    color: var(--white);
+    background-color: var(--dark);
+
+  }
 </style>
---
+++ src/components/Display.vue
@@ -0,0 +1,121 @@
+<template>
+  <b-container>
+    <b-row align-v="center">
+      <b-col class="col-md-auto text-center">
+        <b-row  class="clock">
+          {{ clock }}
+        </b-row>
+      </b-col>
+      <b-col class="col-md-auto text-center">
+        <b-row class="seconds">
+          {{ seconds }}
+        </b-row>
+        <b-row class="date">
+          {{ day }}
+        </b-row>
+        <b-row class="date">
+          {{ date }}
+        </b-row>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  const monthNames = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+    "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
+  ];
+  const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      clock: "",
+      date: "",
+      seconds: "",
+      day: "",
+    }
+  },
+  created() {
+    setInterval(this.getNow, 1000);
+  },
+  methods: {
+    getNow: function() {
+      const now = new Date();
+      const hours = `${now.getHours() < 10 ? '0' : ''}${now.getHours()}`;
+      const minutes = `${now.getMinutes() < 10 ? '0' : ''}${now.getMinutes()}`;
+      this.seconds = `${now.getSeconds() < 10 ? '0' : ''}${now.getSeconds()}`;
+      this.clock = `${hours}:${minutes}`;
+      this.date = `${monthNames[now.getMonth()]} ${now.getDate()}`;
+      this.day = days[now.getDay()];
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .centered {
+    text-align: center;
+  }
+
+  .clock {
+    font-size: 200pt;
+    font-weight: 700;
+    border-right: 3pt solid #ffffff;
+  }
+
+  .date {
+    font-size: 25pt;
+    font-weight: 700;
+    margin-left: 1pt;
+  }
+
+  .seconds {
+    font-size: 70pt;
+    font-weight: 700;
+    margin-left: 1pt;
+  }
+
+  .push-down {
+    margin-top: 50px;
+  }
+
+  .conditions {
+    font-size: 140pt;
+    margin-top: 30px;
+  }
+
+  .wrapper {
+    width: 1200px;
+    margin: 160px auto;
+  }
+
+  .current-temp {
+    font-size: 80pt;
+  }
+
+  .hilo-temp {
+    font-size: 40pt;
+  }
+
+  body, a {
+    color: #555;
+  }
+
+  .sun {
+    font-size: 40pt;
+  }
+
+  .sun .wi {
+    font-size: 70pt;
+    margin: 30px 0;
+  }
+
+  .img-responsive {
+    max-height: 330px;
+  }
+</style>
