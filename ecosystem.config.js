bff6a97f22e7a127af09c71fc9fd55650040f4b7
Kareem Aljabr <kareem.aljabr@universalorlando.com>
2020-06-25 12:39:19 -0400

    pm2 config added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
             script: "./node_modules/@vue/cli-service/bin/vue-cli-service.js",
             ignore_watch: ['node_modules', 'logs'],
             args: "serve",
+            instance_var: 'INSTANCE_ID',
             env: {
                 PORT: 8080,
                 NODE_ENV: 'production',
